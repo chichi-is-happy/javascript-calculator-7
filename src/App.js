a26fe4d736d4886c609f749cdfecdda53e4df3cc
chichi <seonrye.yun@gmail.com>
2024-10-21 22:22:55 +0900

    feat: 프로그램 실행 시 안내 문구 출력
    * 프로그램 실행 시 “덧셈할 문자열을 입력해 주세요.” 문구를 출력한다
    * 입력값 및 결과를 리턴할 수 있도록 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { PROMPT_MESSAGES } from './constants/messages.js';
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const inputs = await Console.readLineAsync(
+        PROMPT_MESSAGES.INPUT_ADDITION_STRING
+      );
+      const formattedInput = inputs.replace('\\n', '\n').trim();
+      const result = this.calculator(formattedInput);
+      return await Console.print(result);
+    } catch (error) {
+      Console.print(`${error.message}`);
+      throw error;
+    }
+  }
+
+  calculator(input) {
+    return `결과 : ${input}`;
+  }
 }
 
 export default App;
---
+++ src/constants/messages.js
@@ -0,0 +1,3 @@
+export const PROMPT_MESSAGES = {
+  INPUT_ADDITION_STRING: '덧셈할 문자열을 입력해 주세요.\n',
+};
