f22b01e0e6d12572bd63d85b661dc431d2a904a6
chichi <seonrye.yun@gmail.com>
2024-10-21 22:51:12 +0900

    feat: 추출된 숫자 합산 함수 추가
    * 추출된 숫자 문자열을 숫자로 변환한 뒤 합한 값을 리턴할 수 있도록 적용

--- src/App.js
+++ src/App.js
@@ -23,10 +23,12 @@ class App {
     if (this.isDefaultDelimiter(input)) {
       console.log('디폴트 구분자');
       const numbers = this.splitByDefaultDelimiter(input);
+      const result = this.sumNumbers(numbers);
       return `결과 : ${result}`;
     } else if (this.isCustomDelimiter(input)) {
       console.log('커스텀 구분자');
       const numbers = this.splitByCustomDelimiter(input);
+      const result = this.sumNumbers(numbers);
       return `결과 : ${result}`;
     }
   }
@@ -89,6 +91,13 @@ class App {
 
   getSplitString(string, delimiter) {
     return string.split(delimiter);
+  }
+
+  sumNumbers(numbers) {
+    const result = numbers
+      .map((number) => Number(number))
+      .reduce((acc, cur) => acc + cur, 0);
+    return result;
   }
 
   validateInput(input) {
